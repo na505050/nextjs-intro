2ff7d7f54cf957b80f28aaee827c288a1ad55be6
na505050 <master@na50.xyz>
2022-06-19 11:25:43 +0900

    log in getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -62,6 +62,7 @@ export default function Home({ results }) {
 
 export async function getServerSideProps() {
   const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  console.log(results);
 
   return {
     props: {
